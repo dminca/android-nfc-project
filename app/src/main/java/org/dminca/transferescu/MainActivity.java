58b15f6a12253866e79d747ab64b5ab41a6902e3
Daniel Andrei Minca <mandrei17@gmail.com>
2018-05-27 12:16:50 +0300

    Implement the file encryption method
    Signed-off-by: Daniel Andrei Minca <mandrei17@gmail.com>

--- app/src/main/java/org/dminca/transferescu/MainActivity.java
+++ app/src/main/java/org/dminca/transferescu/MainActivity.java
@@ -5,14 +5,25 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.nfc.NfcAdapter;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Environment;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 
 public class MainActivity extends AppCompatActivity {
     private NfcAdapter nfcAdapter;
@@ -55,13 +66,51 @@ public class MainActivity extends AppCompatActivity {
             // NFC & Android Beam are supported
 
             // File to be transfered
-            // TODO: Add `wallpaper.jpg` in the Downloads directory before this
-            String fileName = "wallpaper.jpg";
+            String fileName = "sample.txt";
 
             // retrieve path to public Downloads directory
             File fileDirectory = Environment.getExternalStoragePublicDirectory(
                     Environment.DIRECTORY_DOWNLOADS
             );
+
+            // create file with text
+            try {
+                final String TESTSTRING = new String("Hello Android");
+                FileOutputStream fOut = openFileOutput(fileName,
+                        MODE_PRIVATE);
+                OutputStreamWriter osw = new OutputStreamWriter(fOut);
+                FileInputStream fIn = openFileInput(fileName);
+
+                SecretKeySpec sks = new SecretKeySpec("randompass".getBytes(),"AES");
+
+                // create cipher
+                Cipher cipher = null;
+                try {
+                    cipher = Cipher.getInstance("AES");
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                } catch (NoSuchPaddingException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    cipher.init(Cipher.ENCRYPT_MODE, sks);
+                } catch (InvalidKeyException e) {
+                    e.printStackTrace();
+                }
+                CipherOutputStream cos = new CipherOutputStream(fOut, cipher);
+
+                // write bytes
+                int b;
+                byte[] d = new byte[8];
+                while ((b = fIn.read(d)) != -1) {
+                    cos.write(d, 0, b);
+                }
+
+                osw.flush();
+                osw.close();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
 
             // create a new file using the specified dir and name
             File fileToTransfer = new File(fileDirectory, fileName);
